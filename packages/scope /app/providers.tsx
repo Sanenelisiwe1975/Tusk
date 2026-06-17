00fd22ec21e837125d12ab0121362f821163e68a
Maite Lemekoana <mlemekoana13@gmail.com>
2026-06-17 09:11:58 +0200

    create files in scope folder

---
+++ packages/scope /app/providers.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+const client = new QueryClient();
+
+export function Providers ({children}): PropsWithChildren {
+    return(
+        <QueryClientProvider client = {client}> 
+           {children}
+            </QueryClientProvider>
+    );
+
+}
